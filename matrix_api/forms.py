177898404f78caf1a3e1c7e669dcc364926e1e35
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-09 22:20:01 +0300

    add validations for models and forms

--- matrix_api/forms.py
+++ matrix_api/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
-from .models import Matrix
+from matrix_api.models import Matrix, validations_matrix
 
 
 class MatrixForm(forms.ModelForm):
@@ -11,24 +10,4 @@ class MatrixForm(forms.ModelForm):
 
     def clean_matrix_text(self):
         matrix = self.cleaned_data["matrix_text"]
-
-        lines = matrix.split("\n")
-        if not lines:
-            raise ValidationError("Matrix data cannot be empty")
-
-        rows = []
-        for line in lines:
-            numbers = [int(num) for num in line.split() if num.isdigit()]
-            if numbers:
-                rows.append(numbers)
-
-        row_lengths = set(len(row) for row in rows)
-        if len(row_lengths) != 1:
-            raise ValidationError("Matrix rows must have the same number of columns")
-
-        for row in rows:
-            for cell in row:
-                if not isinstance(cell, int):
-                    raise ValidationError("Matrix data must contain only integers")
-
-        return matrix
+        return validations_matrix(matrix)
