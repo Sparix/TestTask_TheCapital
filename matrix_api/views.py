ec0b61a3cea60a9fbfc08025ae22f89369b1f238
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-10 10:55:50 +0300

    change api to router in views

--- matrix_api/views.py
+++ matrix_api/views.py
@@ -1,10 +1,11 @@
 from typing import List
 
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Router
 from matrix_api.matrix import get_matrix, parse_matrix
 from matrix_api.models import Matrix
 
 api = NinjaAPI()
+router = Router()
 
 URL = (
     "https://raw.githubusercontent.com/"
@@ -13,13 +14,13 @@ URL = (
 )
 
 
-@api.get("/matrix-traversal-url/", response=List[int])
+@router.get("/matrix-traversal-url/", response=List[int])
 async def traverse_matrix_by_url(request, url: str = URL) -> List[int]:
     matrix = await get_matrix(url)
     return matrix
 
 
-@api.get("/matrix-traversal-db/", response=List[List[int]])
+@router.get("/matrix-traversal-db/", response=List[List[int]])
 def traverse_matrix_db(request) -> list[list[int]]:
     all_matrix = Matrix.objects.all()
     traverse = [parse_matrix(matrix.matrix_text) for matrix in all_matrix]
