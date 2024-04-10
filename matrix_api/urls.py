e7078a255840e9a9df98858f79f06cceb5858125
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-10 10:54:30 +0300

    add namespace, and add router

--- matrix_api/urls.py
+++ matrix_api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from matrix_api.views import api
+from ninja import NinjaAPI
+from matrix_api.views import router
+
+api = NinjaAPI()
+
+api.add_router("/matrix/", router)  # You can add a router as an object
 
 urlpatterns = [
-    path("matrix/", api.urls),
+    path("matrix/", api.urls, name="matrix"),
 ]
+
+app_name = "matrix"
