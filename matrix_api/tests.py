15afef6ed1fe29df1708f3712cb8e1e902442633
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-10 10:57:10 +0300

    write tests for admin panel, and test views

--- matrix_api/tests.py
+++ matrix_api/tests.py
@@ -1,12 +1,13 @@
 from django.contrib.admin import AdminSite
-from unittest.mock import patch
-from ninja.testing import TestClient
-from matrix_api.views import api as matrix_api
 from django.test import TestCase
+from django.urls import reverse
+from ninja.testing import TestClient
+from httpx import AsyncClient
 
 from matrix_api.admin import MatrixAdmin
 from matrix_api.forms import MatrixForm
 from matrix_api.models import Matrix
+from matrix_api.views import api, router
 
 
 class MatrixFormTestCase(TestCase):
@@ -60,3 +61,60 @@ class MatrixAdminTestCase(TestCase):
 
     def test_matrix_admin_form(self):
         self.assertEqual(self.matrix_admin.form, MatrixForm)
+
+    def test_save_matrix(self):
+        matrix = {
+            "matrix_text": "1 2 4 \n 3 4 5 \n 6 7 8"
+        }
+        form = MatrixForm(data=matrix)
+        self.assertTrue(form.is_valid())
+        form.save()
+        saved_matrix = Matrix.objects.first()
+        self.assertEqual(saved_matrix.matrix_text, matrix["matrix_text"])
+
+
+class TraverseMatrixDb(TestCase):
+    def setUp(self):
+        self.client = TestClient(router)
+
+    def test_matrix_save_db(self):
+        form_data = {
+            "matrix_text":
+                "+-----+-----+-----+-----+\n"
+                "|  10 |  20 |  30 |  40 |\n"
+                "+-----+-----+-----+-----+\n"
+                "|  50 |  60 |  70 |  80 |\n"
+                "+-----+-----+-----+-----+\n"
+                "|  90 | 100 | 110 | 120 |\n"
+                "+-----+-----+-----+-----+\n"
+                "| 130 | 140 | 150 | 160 |\n"
+                "+-----+-----+-----+-----+"
+        }
+        Matrix.objects.create(**form_data)
+        response = self.client.get("/matrix-traversal-db/")
+        traversal = [
+            10, 50, 90, 130,
+            140, 150, 160, 120,
+            80, 40, 30, 20,
+            60, 100, 110, 70,
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(traversal, response.json())
+
+    def test_matrix_db(self):
+        matrix1 = {
+            "matrix_text": "1 2 \n 2 3"
+        }
+        matrix2 = {
+            "matrix_text": "1 2 3 4\n 5 6 7 8\n 9 10 11 12\n 13 14 15 16"
+        }
+        Matrix.objects.create(**matrix1)
+        Matrix.objects.create(**matrix2)
+        response = self.client.get("/matrix-traversal-db/")
+        result = [
+            [1, 2, 3, 2],
+            [1, 5, 9, 13, 14, 15, 16, 12, 8, 4, 3, 2, 6, 10, 11, 7]
+        ]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result, response.json())
